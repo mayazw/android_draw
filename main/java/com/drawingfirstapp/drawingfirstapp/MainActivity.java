dca6b3a6be029aa7cf2daaaea0e93cbdc3367515
mayazw <maya.zweigel@gmail.com>
2016-07-21 17:29:27 +0300

    Add files via upload

---
+++ main/java/com/drawingfirstapp/drawingfirstapp/MainActivity.java
@@ -0,0 +1,113 @@
+package com.drawingfirstapp.drawingfirstapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import java.util.UUID;
+import android.provider.MediaStore;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
+
+public class MainActivity extends Activity implements OnClickListener{
+
+    private DrawingView drawView;
+    private ImageButton currPaint, drawBtn, fillBtn;
+    private float smallBrush, mediumBrush, largeBrush;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        drawView = (DrawingView)findViewById(R.id.drawing);
+        LinearLayout paintLayout = (LinearLayout)findViewById(R.id.paint_colors);
+        currPaint = (ImageButton)paintLayout.getChildAt(0);
+        currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
+
+        smallBrush = getResources().getInteger(R.integer.small_size);
+        mediumBrush = getResources().getInteger(R.integer.medium_size);
+        largeBrush = getResources().getInteger(R.integer.large_size);
+        drawBtn = (ImageButton)findViewById(R.id.draw_btn);
+        drawBtn.setOnClickListener(this);
+        drawView.setBrushSize(mediumBrush);
+
+        fillBtn = (ImageButton)findViewById(R.id.fill_btn);
+        fillBtn.setOnClickListener(this);
+    }
+
+    public void paintClicked(View view){
+        //use chosen color
+        if(view!=currPaint){
+        //update color
+            ImageButton imgView = (ImageButton)view;
+            String color = view.getTag().toString();
+            drawView.setColor(color);
+
+            imgView.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
+            currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint));
+            currPaint=(ImageButton)view;
+        }
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId()==R.id.draw_btn){
+            //draw button clicked
+            final Dialog brushDialog = new Dialog(this);
+            brushDialog.setTitle("Brush size:");
+            brushDialog.setContentView(R.layout.brush_chooser);
+
+            ImageButton smallBtn = (ImageButton)brushDialog.findViewById(R.id.small_brush);
+            smallBtn.setOnClickListener(new OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    drawView.setBrushSize(smallBrush);
+                    drawView.setLastBrushSize(smallBrush);
+                    brushDialog.dismiss();
+                }
+            });
+
+            ImageButton mediumBtn = (ImageButton)brushDialog.findViewById(R.id.medium_brush);
+            mediumBtn.setOnClickListener(new OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    drawView.setBrushSize(mediumBrush);
+                    drawView.setLastBrushSize(mediumBrush);
+                    brushDialog.dismiss();
+                }
+            });
+
+            ImageButton largeBtn = (ImageButton)brushDialog.findViewById(R.id.large_brush);
+            largeBtn.setOnClickListener(new OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    drawView.setBrushSize(largeBrush);
+                    drawView.setLastBrushSize(largeBrush);
+                    brushDialog.dismiss();
+                }
+            });
+
+            brushDialog.show();
+        }
+        //Bucket button clicked
+        if(view.getId() == R.id.fill_btn){
+            ImageButton bucket = (ImageButton)findViewById(R.id.fill_btn);
+            bucket.setOnClickListener(new OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    String color = v.getTag().toString();
+                    drawView.setFill(color);
+                }
+            });
+        }
+    }
+
+
+}
